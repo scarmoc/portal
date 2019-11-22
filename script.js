6450ab183c1b2b89ecd254ad64fab7457c6fa642
Robert <geicarobert@gmail.com>
2019-11-22 16:03:08 +0200

    template literals

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
-function showGrupe() {
-	//document.getElementById("demo").innerHTML = "Hello World";
-	console.log("Fct Mere");
-}
+function grupa1Info() {
+    const orar = `                
+		<table style="widtd:100%">
+			<tr>
+				<th>Ora</th>
+				<th>Luni</th>
+				<th>Marti</th>
+				<th>Miercuri</th>
+				<th>Joi</th>
+				<th>vineri</th>
+			</tr>
+			<tr>
+				<td>08-10</td>
+				<td></td>
+				<td>Calcul</td>
+				<td>-</td>
+				<td>-</td>
+				<td>Logica</td>
+			</tr>
+			<tr>
+				<td>10-12</td>
+				<td>Fai</td>
+				<td>Fundamentele progr</td>
+				<td>-</td>
+				<td>-</td>
+				<td>Logica</td>
+			</tr>
+			<tr>
+				<td>12-14</td>
+				<td>Fai</td>
+				<td>Arhitectura</td>
+				<td>-</td>
+				<td>-</td>
+				<td>Logica</td>
+			</tr>
+			<tr>
+				<td>14-16</td>
+				<td>Fai</td>
+				<td>-</td>
+				<td>Engleza</td>
+				<td>-</td>
+				<td>Logica</td>
+			</tr>
+			<tr>
+				<td>16-18</td>
+				<td>Fai</td>
+				<td>-</td>
+				<td>-</td>
+				<td>-</td>
+				<td>Logica</td>
+			</tr>
+		</table>
+	
+    `;
+    
+    document.getElementById('grupa1OrarInfo').innerHTML += orar;
+    
+};
+
